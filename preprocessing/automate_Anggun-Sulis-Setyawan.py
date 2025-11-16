c8fa6f9ec0a8a029eb56f9abec3cf6bc1589c647
Sulbae <assulis500@gmail.com>
2025-11-16 16:10:51 +0700

    update automate.py

--- preprocessing/automate_Anggun-Sulis-Setyawan.py
+++ preprocessing/automate_Anggun-Sulis-Setyawan.py
@@ -4,10 +4,22 @@ from joblib import dump
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+import os
+import logging
 
-def preprocess_data(data):
-    import logging
+def preprocess_data():
+
     logging.basicConfig(level=logging.INFO)
+
+    # Dataset Source
+    data_folder = "../dataset_raw"
+    file_name = "water_potability_raw.csv"
+
+    data_path = os.path.join(data_folder, file_name)
+    logging.info(f"Dataset berhasil di-load dari {data_path}")
+
+    # Load dataset
+    data = pd.read_csv(data_path)
 
     # Pisahkan fitur dan target
     X = data.drop(columns=['Potability'])
